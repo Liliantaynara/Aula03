fe55ffd663614fc3322d3fac2963b9305d380e37
Lilian Taynara <liliantaynaramariadasilva@gmail.com>
2025-04-21 21:55:26 -0300

    primeiro

---
+++ 0002.py
@@ -0,0 +1,22 @@
+h1 = int(input("digite horas: "))
+m1 = int(input("digite os minutos: "))
+h2 = int(input("digite novamente horas: "))
+m2 = int(input("digite novamente horas: "))
+
+totalh1 = 12 or 24
+totalmin = 60
+
+if h1 > 12:
+    h2 -= 24
+
+    m1 > 60
+    m2 -= 60
+
+    totalh1 = h1 + m2
+    totalmin = m1 + m2
+
+if totalmin >= 60:
+    totalh1 += totalmin // 60
+    totalmin = totalh1 % 60
+
+print(f"horario {totalh1}H e {totalmin}Min definida")
---
+++ 0005.py
@@ -0,0 +1,14 @@
+n1 = float (input( "digite nota 1: "))
+n2 = float (input("Digite nota 2: "))
+n3 = float (input("diigite nota3: "))
+
+media = ( n1 + n2 + n3)/3
+
+if media >= 7:
+    print(f"aprovado {media}")
+
+else:
+    if media <=4:
+        print(f"recuperacao{media}")
+    else:
+        print(f"vc foi reprovado {media}")
